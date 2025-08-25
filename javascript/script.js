d07462fe7f506e4cdd02d46d0f996e3a41093e51
Lilya3 <bazaralily4@gmail.com>
2025-08-25 12:09:14 +0200

    js : carroussel + dots  + html + modif css arrow dots

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// 1. Données
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,3 +17,79 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// 2. Sélection DOM
+const bannerImg = document.querySelector(".banner-img");
+const tagLine = document.querySelector(".tag-line");
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+const dots = Array.from(document.querySelectorAll(".dots .dot"))
+
+// 3. Variables globales
+const basePath = "/assets/images/slideshow/";
+let index = 0;
+let timer = null;
+
+// 4. Fonction render (affiche img + text + dots)
+function render() {
+	// a. Pas de rechargement si même image
+	const targetSrc = basePath + slides[index].image;
+	const currentSrc = bannerImg.getAttribute("src");
+
+	if (currentSrc !== targetSrc){
+		bannerImg.setAttribute("src", targetSrc);
+	}
+	// b. Tagline
+	tagLine.innerHTML = slides[index].tagLine;
+
+	// c. Dots : activer la bonne, désactiver les autres
+	dots.forEach(dot => dot.classList.remove("dot_selected"));
+	if (dots[index]) dots[index].classList.add("dot_selected");
+}
+
+// 5. Fonctions navigation
+function next(){
+	index = (index + 1) % slides.length;
+	render();
+}
+
+function prev() {
+	index = (index - 1 + slides.length) % slides.length;
+	render();
+}
+
+// 6. Autoplay
+function startAutoplay(intervalMs = 3000){
+	stopAutoplay();
+	timer = setInterval(() => {
+		index = (index + 1) % slides.length;
+		render();
+	}, intervalMs);
+}
+
+function stopAutoplay() {
+	if (timer) {
+		clearInterval(timer);
+		timer = null;
+	}
+}
+
+// 7. Événements
+rightArrow.addEventListener("click", next);
+leftArrow.addEventListener("click", prev);
+
+dots.forEach((dot, i) => {
+	dot.addEventListener("click", () => {
+		index = i;
+		render();
+	});
+});
+
+// Arrêt autoplay dès user interagit
+[rightArrow, leftArrow, ...dots].forEach(el =>
+	el.addEventListener("click", stopAutoplay));
+
+
+// 8. Premier lancement 
+render();
+startAutoplay(3000);
